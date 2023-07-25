b433ba6b227a0d3f0a16cb45835fa34fb90df906
minhyeok <minhyeokbaek100@gmail.com>
2023-07-26 00:22:24 +0900

    0725스터디

--- 0724리액트심화/App.js
+++ 0724리액트심화/App.js
@@ -114,3 +114,65 @@ const List = styled.ul`
 const ListItem = styled.li` padding: 20px 100px; background: orangered; color: white;
 &+& { margin-top: 8px;
 } `;
+
+
+
+
+
+//JSX를 사용하면 코드의 재사용성을 높여, 개발자의 생산성이 높아진다.
+//Hook이 없을 때는 함수형 컴포넌트를 이용해 State와 생명주기를 관리해야한다
+function App() {
+  return <div>
+      <p>엘리스 식당에 오신 것을 환영합니다.</p>
+  </div>
+}
+
+const Menu = () => {
+  return <div>
+      <ul>
+      <h3>엘리스 식당 메뉴판</h3>
+          <li>짜장면</li>
+          <li>짬뽕</li>
+          <li>볶음밥</li>
+      </ul>
+  </div>
+}
+
+const Order = (props) => {
+  return <p>사장님, {props.menu} {props.count}개 주세요!</p>
+}
+
+//결제를 위한 Pay 컴포넌트를 완성하세요.
+function Pay() {
+  const handleClick = () => {
+    alert("결제가 완료되었습니다.");
+  }
+  return <button onClick={handleClick}> 결제하기 </button>;
+}
+
+//4
+function App() {
+  const [person, setPerson] = useState({
+    name: "",
+    age: 20,
+    school: ""
+  })
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setPerson((current) => {
+      const newPerson = {...current};
+      newPerson[name] = value;
+      return newPerson;
+    })
+  }
+  return (
+    <div className="App">
+      <input name="name" value={person.name} onChange={handleChange} />     
+      <input name="age" value={person.age} onChange={handleChange} />
+      <input name="school" value={person.school} onChange={handleChange} />
+      <p>
+        {person.name}님은 {person.school}에 재학 중이며 현재 {person.age}세입니다.
+      </p>
+    </div>
+  );
+}
