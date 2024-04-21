c1ab875f79e10ee9017cceaf7a17097b413590e0
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-04-21 14:57:50 +0900

    hook-form test

---
+++ 2023.04.11-project-test/project-test/src/components/WrongCompoundForm/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { IconType } from "react-icons";
+import classNames from "classnames";
+import styles from "./index.module.scss";
+
+interface ButtonProps {
+    name?: string; // 버튼 텍스트
+    icon?: IconType; // 아이콘 버튼
+    onClick?: (() => Promise<void>) | (() => void);
+    type?: "submit" | "button";
+    variant?: "light" | "dark" | "icon"; // 버튼 색상
+    size?: "default" | "medium";
+}
+
+export const Button = ({
+    name,
+    icon: Icon,
+    onClick,
+    type,
+    variant,
+    size = "default",
+}: ButtonProps) => {
+    const buttonClasses = classNames(styles.container, {
+        [styles.light]: variant === "light",
+        [styles.dark]: variant === "dark",
+        [styles.icon]: variant === "icon",
+        [styles.medium]: size === "medium",
+        [styles[size]]: size !== "default",
+    });
+
+    return (
+        <button className={buttonClasses} type={type} onClick={onClick}>
+            {Icon && <Icon />}
+            {name}
+        </button>
+    );
+};
---
+++ 2023.04.11-project-test/project-test/src/components/WrongCompoundForm/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import * as yup from "yup";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { FormContext } from "@hooks/useFormContext";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { RadioButton } from "./RadioButton/RadioButton";
+
+interface FormProps {
+    children: React.ReactNode;
+    onSubmit: SubmitHandler<FieldValues>; // 폼 제출 핸들러
+    schema?: yup.ObjectSchema<FieldValues>; // 폼 유효성 검사 스키마
+    className?: string;
+}
+
+export const Form = ({ children, onSubmit, schema, className }: FormProps) => {
+    // 로딩 상태 변경 핸들러
+    const [isLoading, setIsLoading] = useState(false);
+    const loadingHandler = (boolean: boolean) => setIsLoading(boolean);
+
+    // useForm 훅을 사용하여 폼 상태 및 유효성 검사 처리
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        resolver: schema ? yupResolver(schema) : undefined,
+        mode: "onChange",
+    });
+
+    // 제출 핸들러 함수
+    const submit: SubmitHandler<FieldValues> = async (data) => {
+        setIsLoading(true);
+        try {
+            await onSubmit(data);
+        } catch (error) {
+            console.error("submit error", error);
+            setIsLoading(false);
+        }
+    };
+    return (
+        <FormContext.Provider
+            value={{ isLoading, errors, register, loadingHandler }}
+        >
+            <form className={className} onSubmit={handleSubmit(submit)}>
+                {children}
+            </form>
+        </FormContext.Provider>
+    );
+};
+Form.Input = Input;
+Form.Button = Button;
+Form.RadioButton = RadioButton;
